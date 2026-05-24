4ed646f5d0e2bf9f1db5c9250c8559ecd60d0319
pratikbal298 <pratikbal10b28@gmail.com>
2026-05-24 12:02:09 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public String encode(List<String> strs) {
+        
+        if(strs.size()==0 ){
+            return Character.toString((char)258);
+        }
+        String separate = Character.toString((char)257);
+        StringBuilder sb = new StringBuilder();
+        for(String s:strs){
+            sb.append(s);
+            sb.append(separate);
+        }
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+        
+    }
+    public List<String> decode(String s) {
+        if(s.equals(Character.toString((char)258))){
+            return new ArrayList();
+        }
+       String separate = Character.toString((char)257);
+         return Arrays.asList(s.split(separate, -1));
+    }
+}
+
